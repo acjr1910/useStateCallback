fcc22b1389282c408d37562716b699961dd62a41
Agnaldo Cardoso Junior <acjunior.k2@rd.com.br>
2020-10-05 23:25:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### useStateCallback
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/useStateCallback.js",
+  output: {
+    path: path.resolve("lib"),
+    filename: "useStateCallback.js",
+    libraryTarget: "commonjs2",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+};
